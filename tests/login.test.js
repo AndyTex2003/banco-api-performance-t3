ea9a11d8d5fae2a4ff85403cb7ed5e005835fdf3
tex batista <andersonrexbatista@gmail.com>
2026-03-20 09:33:30 -0300

    Adicionando VUs e Duration para execução do teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -3,9 +3,10 @@ import { sleep, check } from 'k6';
 
 export const options = {
     
-    iterations: 10,
+    vus: 10,
+    duration: '30s',
     thresholds: {
-      http_req_duration:  ['p(90)<10', 'max<7'],
+      http_req_duration:  ['p(90)<3000', 'max<5000'],
       http_req_failed: ['rate<0.01']
     }
 };
@@ -34,4 +35,4 @@ export default function () {
     });
 
     sleep(1);
-};
+};  
