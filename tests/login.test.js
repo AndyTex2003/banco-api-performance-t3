f5f8189dfa9f23c5f9e87a6286b0eebf65a74671
tex batista <andersonrexbatista@gmail.com>
2026-03-24 12:22:57 -0300

    Criando um arquivo de configuração e aplicando variáveis de ambiente

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
+import { pegarBaseURL } from '../utils/variaveis.js';
     // JSON.parse permite pegar um texto do open e converte-lo em um objeto Javascript
 const postLogin = JSON.parse(open('../fixtures/postLogin.json'))
 
@@ -21,7 +22,7 @@ export const options = {
 
 export default function () {
   // Faça uma solicitação POST para o URL de destino.  
-    const url = 'http://localhost:3000/login';
+    const url = pegarBaseURL() + 'login';
     
     const payload = JSON.stringify(postLogin);
 
